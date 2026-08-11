08eb43f0b06acb194338619c94f3a711d4620efa
bilgisistemlericaytakip-a11y <bilgisistemleri.caytakip@gmail.com>
2026-08-11 15:28:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='caytakip-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}).catch(function(){}));});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener('fetch',function(e){
+  var url=e.request.url;
+  if(url.indexOf('supabase.co')>-1||url.indexOf('cdn.jsdelivr.net')>-1)return; // veri/CDN her zaman ağdan
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      if(res&&res.status===200&&url.indexOf(self.location.origin)===0){var cl=res.clone();caches.open(CACHE).then(function(c){c.put(e.request,cl);});}
+      return res;
+    }).catch(function(){return caches.match(e.request).then(function(r){return r||caches.match('./index.html');});})
+  );
+});
